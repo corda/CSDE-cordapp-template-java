1fb18290ca783498e459726b031f2c0f8e35f8f5
mattbradburyr3 <matthew.bradbury@r3.com>
2023-01-29 17:23:49 +0000

    tidy up

--- workflows/src/main/java/com/r3/developers/csdetemplate/utilities/CorDappHelpers.java
+++ workflows/src/main/java/com/r3/developers/csdetemplate/utilities/CorDappHelpers.java
@@ -6,18 +6,18 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-//
-//public final class CorDappHelpers {
-//    public static <T> T findAndExpectExactlyOne(Collection<T> collection, Predicate<? super T> filterFn, String exceptionMsg)
-//    {
-//        Collection<T> results = collection.stream().filter(filterFn).collect(Collectors.toList());
-//        if(results.size() != 1){
-//            throw new RuntimeException(exceptionMsg);
-//        }
-//        return results.iterator().next();
-//    }
-//
-//    public static <T> T findAndExpectExactlyOne(Collection<T> collection, String exceptionMsg) {
-//        return findAndExpectExactlyOne(collection, e -> true, exceptionMsg);
-//    }
-//}
+
+public final class CorDappHelpers {
+    public static <T> T findAndExpectExactlyOne(Collection<T> collection, Predicate<? super T> filterFn, String exceptionMsg)
+    {
+        Collection<T> results = collection.stream().filter(filterFn).collect(Collectors.toList());
+        if(results.size() != 1){
+            throw new RuntimeException(exceptionMsg);
+        }
+        return results.iterator().next();
+    }
+
+    public static <T> T findAndExpectExactlyOne(Collection<T> collection, String exceptionMsg) {
+        return findAndExpectExactlyOne(collection, e -> true, exceptionMsg);
+    }
+}
